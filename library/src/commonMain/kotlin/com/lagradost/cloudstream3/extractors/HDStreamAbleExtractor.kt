29ee02373421c7acacde1596a15334ffabcd703e
mahdiridoy <59791566+mahdiridoy@users.noreply.github.com>
2026-09-21 22:39:00 +0600

    Delete library directory

--- library/src/commonMain/kotlin/com/lagradost/cloudstream3/extractors/HDStreamAbleExtractor.kt
+++
@@ -1,8 +0,0 @@
-// ! Bu araç @keyiflerolsun tarafindan | @KekikAkademi için yazilmistir.
-
-package com.lagradost.cloudstream3.extractors
-
-class HDStreamAble : PeaceMakerst() {
-    override var name    = "HDStreamAble"
-    override var mainUrl = "https://hdstreamable.com"
-}
